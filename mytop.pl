e47ae98337ad56f1d115ff0fde4d1beb69d55458
范野人 <fanyeren@gmail.com>
2013-08-29 18:12:24 +0800

    Update mytop.pl

--- mytop.pl
+++ mytop.pl
@@ -694,11 +694,11 @@ sub processlist_all {
 sub find_bin_in_path {
     my $bin = shift;
     next unless $bin;
- 
+
     use File::Spec::Functions qw(catfile);
-    
+
     my @paths = split(/:/, $ENV{PATH});
-    
+
     for my $inc_dir (@paths) {
         my $bin_in_path = catfile($inc_dir, $bin);
         return $bin_in_path if -e $bin_in_path && -x $bin_in_path;
